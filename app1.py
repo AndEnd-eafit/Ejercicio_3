997de38096c5de77dbda59b3214e9d8bcb64a6df
AndEnd-eafit <aprivasm@eafit.edu.co>
2024-10-02 22:58:02 -0500

    Update app1.py

--- app1.py
+++ app1.py
@@ -9,13 +9,13 @@ st.markdown("""
     <style>
     @import url('https://fonts.googleapis.com/css2?family=Lexend:wght@400;700&display=swap');
     @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
-    
+
     .title {
         font-family: 'Lexend', sans-serif;
         font-weight: 700;
         text-align: center;
     }
-    
+
     .text {
         font-family: 'Inter', sans-serif;
         text-align: justify;
@@ -29,12 +29,14 @@ st.markdown("""
     """, unsafe_allow_html=True)
 
 # Título
-st.markdown('<h1 class="title">Reconocimiento de caracteres</h1>', unsafe_allow_html=True)
+st.markdown('<h1 class="title">Reconocimiento óptico de Caracteres</h1>', unsafe_allow_html=True)
 
 # Espacio para la imagen (PNG)
-st.markdown('<div class="image-container">', unsafe_allow_html=True)
-img_file_buffer = st.file_uploader("Sube una imagen en formato PNG", type=["png"])
-st.markdown('</div>', unsafe_allow_html=True)
+image = Image.open('Reconocimiento de caracteres - Yoru.png')
+st.image(image, width=300, use_column_width='auto', caption="Imagen del Traductor")
+
+# Captura de imagen desde la cámara
+img_file_buffer = st.camera_input("Toma una Foto")
 
 # Filtro en el sidebar
 with st.sidebar:
@@ -43,19 +45,19 @@ with st.sidebar:
 # Procesamiento de imagen y OCR
 if img_file_buffer is not None:
     # Leer el archivo de imagen
-    img = Image.open(img_file_buffer)
-    cv2_img = np.array(img)
-    
+    bytes_data = img_file_buffer.getvalue()
+    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+
     # Aplicar filtro si es necesario
     if filtro == 'Con Filtro':
         cv2_img = cv2.bitwise_not(cv2_img)
     
     # Convertir la imagen a RGB
     img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
-    
+
     # Extraer texto de la imagen
     text = pytesseract.image_to_string(img_rgb)
-    
+
     # Mostrar el texto extraído
     st.markdown('<p class="text">Texto extraído:</p>', unsafe_allow_html=True)
     st.write(text)
